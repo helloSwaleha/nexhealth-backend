c8035d6d97f6f345748d1a3db5f0b58f7643978d
Swaleha <swalehapatwekar16@gmail.com>
2026-04-11 00:44:55 +0530

    Update BaseEntity.java

--- src/main/java/com/clinic/management/entity/BaseEntity.java
+++ src/main/java/com/clinic/management/entity/BaseEntity.java
@@ -21,7 +21,7 @@ public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-   protected Long id
+   protected Long id;
 
     @Column(updatable = false)
     private LocalDateTime createdAt;
